53fe9fbfcd92327634e144be0e2af9159c6b2792
toddmath <8497474+toddmath@users.noreply.github.com>
2026-08-31 18:16:20 -0700

    removed buggy flowbite logic in Dropdown comp

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import { Dropdown } from "flowbite-react";
+// import { Dropdown } from "flowbite-react";
 // import cn from "classnames"
 
 type DropdownProps = {
@@ -18,15 +18,33 @@ const DropDown: React.FC<DropdownProps> = ({
   onClick,
 }) => {
   return (
-    <Dropdown inline={inline} label={label}>
-      {header && <Dropdown.Header>{header}</Dropdown.Header>}
-      {items.map((item) => (
-        <Dropdown.Item key={String(item)} onClick={onClick}>
-          {item}
-        </Dropdown.Item>
-      ))}
-    </Dropdown>
+    <div className="dropdown">
+      <label tabIndex={0} className="btn m-1">
+        {label}
+      </label>
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        {header && <li className="menu-title">{header}</li>}
+        {items.map((item) => (
+          <li key={String(item)} onClick={onClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
+  // return (
+  //   <Dropdown inline={inline} label={label}>
+  //     {header && <Dropdown.Header>{header}</Dropdown.Header>}
+  //     {items.map((item) => (
+  //       <Dropdown.Item key={String(item)} onClick={onClick}>
+  //         {item}
+  //       </Dropdown.Item>
+  //     ))}
+  //   </Dropdown>
+  // );
 };
 
 export default DropDown;
